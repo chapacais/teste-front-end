4be8421ccfa7c69b20a8da105902c8346bc7b635
Pedro Chapacais <pedro.flch@gmail.com>
2022-05-09 23:50:47 -0300

    added searchbar to menu

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="category">
     <h2 class="category__title">{{ category.category_title }}</h2>
+    <div class="search">
+      <label for="search">Search:</label>
+      <input type="search" name="search" @input="filter = $event.target.value">
+    </div>
     <ul class="category__list">
-      <li v-for="product in category.products" :key="product.id">
+      <li v-for="product in categoryFilter" :key="product.id">
         <Item @open-modal="this.$emit('open-modal', product)" :product="product"/>
       </li>
     </ul>
@@ -19,6 +23,21 @@ export default {
   },
   props: {
     category: Object
+  },
+  data() {
+    return {
+      filter: ''
+    }
+  },
+  computed: {
+    categoryFilter() {
+        if(this.filter) {
+            let exp = new RegExp(this.filter.trim(), 'i');
+            return this.category.products.filter(data => exp.test(data.title));
+        } else {
+            return this.category.products;
+        }
+    }
   },
   emits: ['open-modal']
 }
@@ -44,6 +63,25 @@ export default {
   border-bottom: 1px solid rgba(150, 150, 150, 0.5);
 }
 
+.search {
+  margin-top: 25px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+}
+
+.search label {
+  margin-bottom: 5px;
+}
+
+.search input {
+  border: 1px solid rgba(50, 50, 50, 0.5);
+  border-radius: 5px;
+  padding: 10px;
+  width: 50%;
+}
+
 .category__list {
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -59,12 +97,26 @@ export default {
 }
 
 @media screen and (max-width: 840px) {
+  .search {
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .search label {
+    margin-right: 5px;
+  }
+
   .category__list {
     grid-template-columns: 1fr;
   }
 }
 
 @media screen and (max-width: 475px) {
+  .search input {
+    width: 100%;
+  }
+
   .category {
     padding: 10px 0;
   }
